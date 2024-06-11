fcc3525d2f98e2d42f32bc5405f200194581944f
Adam Mwaniki <adamndegwa@protonmail.com>
2024-06-11 22:40:27 +0300

    Completes code along

--- lib/department.py
+++ lib/department.py
@@ -52,3 +52,23 @@ class Department:
         department = cls(name, location)
         department.save()
         return department
+    
+    def update(self):
+        """Update the table row corresponding to the current Department instance."""
+        sql = """
+            UPDATE departments
+            SET name = ?, location = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.location, self.id))
+        CONN.commit()
+
+    def delete(self):
+        """Delete the table row corresponding to the current Department instance"""
+        sql = """
+            DELETE FROM departments
+            WHERE id = ?
+        """
+
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
